0c6378cc807d656edab0c08e8bb4db845b5e2da6
Ayush Mohapatra <34229225+tummler13@users.noreply.github.com>
2018-06-19 11:21:20 +0530

    Added Workshop

--- app/src/main/java/in/mittt/tt18/network/APIClient.java
+++ app/src/main/java/in/mittt/tt18/network/APIClient.java
@@ -4,23 +4,26 @@ import in.mittt.tt18.models.categories.CategoriesListModel;
 import in.mittt.tt18.models.events.EventsListModel;
 import in.mittt.tt18.models.events.ScheduleListModel;
 import in.mittt.tt18.models.results.ResultsListModel;
+import in.mittt.tt18.models.workshops.WorkshopsListModel;
+import in.mittt.tt18.models.workshops.WorkshopsModel;
 import retrofit2.Call;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 import retrofit2.http.GET;
 
+
 public class APIClient {
     private static Retrofit retrofit = null;
 
-
     private static final String BASE_URL = "https://api.mitportals.in/";
-
 
     public static APIInterface getAPIInterface(){
 
         if (retrofit == null){
-            retrofit = new Retrofit.Builder().baseUrl(BASE_URL).addConverterFactory(GsonConverterFactory.create()).build();
-
+            retrofit = new Retrofit.Builder()
+                    .baseUrl(BASE_URL)
+                    .addConverterFactory(GsonConverterFactory.create())
+                    .build();
 
         }
         return retrofit.create(APIInterface.class);
@@ -38,7 +41,8 @@ public class APIClient {
         @GET("schedule")
         Call<ScheduleListModel> getScheduleList();
 
-
+        @GET("workshops")
+        Call<WorkshopsListModel> getWorkshopsList();
 
     }
 
