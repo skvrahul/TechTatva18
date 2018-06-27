4ef9b647b2f8596f67732499f52277558b9f2991
Ayush Mohapatra <34229225+tummler13@users.noreply.github.com>
2018-06-27 19:14:07 +0530

    Added Workshops Models and Item layouts (#10)
    * Add files via upload
    
    * Delete item_results.xml
    
    * Delete item_event.xml
    
    * Delete item_category.xml
    
    * Delete fragment_workshops.xml
    
    * Delete fragment_schedule.xml
    
    * Delete fragment_home.xml
    
    * Delete fragment_results.xml
    
    * Delete fragment_categories.xml
    
    * Delete activity_main.xml
    
    * Add files via upload
    
    * Added Workshop Models, item_layouts.
    
    * Updated Workshops
    
    * changes
    
    * Added Workshops
    
    * updated workshops
    
    * Added Workshop Adapter
    
    * Add files via upload
    
    * Added Workshop Models
    
    * Added Workshop
    
    * Added Workshop models
    
    * Added workshops

---
+++ app/src/main/java/in/mittt/tt18/models/workshops/WorkshopsListModel.java
@@ -0,0 +1,29 @@
+package in.mittt.tt18.models.workshops;
+
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+
+public class WorkshopsListModel {
+
+
+        @SerializedName("data")
+        @Expose
+        private List<WorkshopsModel> workshopsList = new ArrayList<>();
+
+        public WorkshopsListModel() {
+        }
+
+        public List<WorkshopsModel> getWorkshopsList() {
+            return workshopsList;
+        }
+
+        public void setWorkshopsList(List<WorkshopsModel> workshopsList) {
+            this.workshopsList = workshopsList;
+        }
+    }
+
